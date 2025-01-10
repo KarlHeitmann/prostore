5f323a2ead02c118acc6ef947e2bc30595817d3c
Karl Heitmann <khf1988@gmail.com>
2025-01-10 18:16:57 -0300

    clase 15: Sample products y Product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 // CODE TO TEST LOADER
 // const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
@@ -9,7 +11,16 @@ export const metadata = {
 // const Homepage = async () => {
 //   await delay(2000)
 const Homepage = () => {
-  return <>Prostore</>;
+  console.log(sampleData);
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
