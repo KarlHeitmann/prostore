7a164388a0dbc8d48a2c70103972b8c51d35b00f
Karl Heitmann <khf1988@gmail.com>
2025-01-10 17:46:54 -0300

    clase 13: not found y loading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
+// CODE TO TEST LOADER
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
 export const metadata = {
   title: "HOME",
 };
 
+// CODE TO TEST LOADER
+// const Homepage = async () => {
+//   await delay(2000)
 const Homepage = () => {
   return <>Prostore</>;
 };
